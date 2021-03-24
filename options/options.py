554fefbf6c14efdbb93eebcfd900e7419183a68b
root <P0lyFish>
2021-03-24 05:28:46 +0000

    refactor

--- options/options.py
+++ options/options.py
@@ -45,7 +45,7 @@ def parse(opt_path, is_train=True):
     for key, path in opt["path"].items():
         if path and key in opt["path"] and key != "strict_load":
             opt["path"][key] = osp.expanduser(path)
-    opt["path"]["root"] = osp.abspath(osp.join(__file__, osp.pardir, osp.pardir, osp.pardir))
+    opt["path"]["root"] = osp.abspath(osp.join(__file__, osp.pardir, osp.pardir))
     if is_train:
         experiments_root = osp.join(opt["path"]["root"], "experiments", opt["name"])
         opt["path"]["experiments_root"] = experiments_root
