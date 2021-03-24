a69b36a2e72206a7144add9050a83fb1925e7717
P0lyFish <tranthephong32@gmail.com>
2021-03-24 10:46:00 +0700

    update test_data_augmentation.py

--- test_data_augmentation.py
+++ test_data_augmentation.py
@@ -1,13 +1,15 @@
 import argparse
 import logging
 import os
+import os.path as osp
 import random
-
-import data.util as data_util
+import torch
 import lmdb
 import numpy as np
+import cv2
+
+import data.util as data_util
 import options.options as options
-import torch
 import utils.util as util
 from models.kernel_wizard import KernelWizard
 
@@ -27,22 +29,32 @@ def main():
     parser.add_argument("--source_W", action="store", help="source image width", type=int, default=None)
     parser.add_argument("--target_H", action="store", help="target image height", type=int, default=None)
     parser.add_argument("--target_W", action="store", help="target image width", type=int, default=None)
+    parser.add_arguement("--augmented_H", action="store", help="desired height of the augmented images",
+                         type=int, default=None)
+    parser.add_arguement("--augmented_W", action="store", help="desired width of the augmented images",
+                         type=int, default=None)
 
     parser.add_argument("--model_path", action="store", help="model path", type=str, default=None)
-    parser.add_argument("--LQ_root", action="store", help="low-quality dataroot", type=str, default=None)
-    parser.add_argument("--HQ_root", action="store", help="high-quality dataroot", type=str, default=None)
+    parser.add_argument("--source_LQ_root", action="store", help="source low-quality dataroot", type=str, default=None)
+    parser.add_argument("--source_HQ_root", action="store", help="source high-quality dataroot", type=str, default=None)
+    parser.add_argument("--target_HQ_root", action="store", help="target high-quality dataroot", type=str, default=None)
     parser.add_argument("--save_path", action="store", help="save path", type=str, default=None)
     parser.add_argument("--yml_path", action="store", help="yml path", type=str, default=None)
+    parser.add_argument("--num_images", action="store", help="number of desire augmented images", type=str, default=None)
 
     args = parser.parse_args()
 
     model_path = args.model_path
-    LQ_root = args.LQ_root
-    HQ_root = args.HQ_root
+    source_LQ_root = args.source_LQ_root
+    source_HQ_root = args.source_HQ_root
+    target_HQ_root = args.target_HQ_root
+
     save_path = args.save_path
     source_H, source_W = args.source_H, args.source_W
     target_H, target_W = args.target_H, args.target_W
+    augmented_H, augmented_W = args.augmented_H, args.augmented_W
     yml_path = args.yml_path
+    num_images = args.num_images
 
     # model_path = '../experiments/GOPRO_wsharp_woVAE/models/300000_G.pth'
     # LQ_root = '../datasets/GOPRO/train_blur_linear.lmdb'
@@ -55,8 +67,9 @@ def main():
     logger = logging.getLogger("base")
     os.makedirs(save_path, exist_ok=True)
     util.setup_logger("base", save_path, "test", level=logging.INFO, screen=True, tofile=True)
-    logger.info("LQ root: {}".format(LQ_root))
-    logger.info("HQ root: {}".format(HQ_root))
+    logger.info("source LQ root: {}".format(source_LQ_root))
+    logger.info("source HQ root: {}".format(source_HQ_root))
+    logger.info("target HQ root: {}".format(target_HQ_root))
     logger.info("model path: {}".format(model_path))
 
     # Initializing mode
@@ -69,41 +82,57 @@ def main():
     logger.info("Done")
 
     # processing data
-    HQ_env = lmdb.open(HQ_root, readonly=True, lock=False, readahead=False, meminit=False)
-    LQ_env = lmdb.open(LQ_root, readonly=True, lock=False, readahead=False, meminit=False)
-    paths_HQ, _ = data_util.get_image_paths("lmdb", HQ_root)
-    print(len(paths_HQ))
+    source_HQ_env = lmdb.open(source_HQ_root, readonly=True, lock=False, readahead=False, meminit=False)
+    source_LQ_env = lmdb.open(source_LQ_root, readonly=True, lock=False, readahead=False, meminit=False)
+    target_HQ_env = lmdb.open(target_HQ_root, readonly=True, lock=False, readahead=False, meminit=False)
+    paths_source_HQ, _ = data_util.get_image_paths("lmdb", source_HQ_root)
+    paths_target_HQ, _ = data_util.get_image_paths("lmdb", target_HQ_root)
 
     psnr_avg = 0
 
-    num_samples = 100
+    for i in range(num_images):
+        source_key = np.random.choice(paths_source_HQ)
+        target_key = np.random.choice(paths_target_HQ)
 
-    for i in range(num_samples):
-        key = np.random.choice(paths_HQ)
+        source_rnd_h = random.randint(0, max(0, source_H - augmented_H))
+        source_rnd_w = random.randint(0, max(0, source_W - augmented_W))
+        target_rnd_h = random.randint(0, max(0, target_H - augmented_H))
+        target_rnd_w = random.randint(0, max(0, target_W - augmented_W))
 
-        rnd_h = random.randint(0, max(0, source_H - target_H))
-        rnd_w = random.randint(0, max(0, source_W - target_W))
-
-        source_LQ = read_image(LQ_env, key, rnd_h, rnd_w, target_H, target_W)
-        source_HQ = read_image(HQ_env, key, rnd_h, rnd_w, target_H, target_W)
+        source_LQ = read_image(source_LQ_env, source_key, source_rnd_h, source_rnd_w, augmented_H, augmented_W)
+        source_HQ = read_image(source_HQ_env, source_key, source_rnd_h, source_rnd_w, augmented_H, augmented_W)
+        target_HQ = read_image(target_HQ_env, target_key, target_rnd_h, target_rnd_w, augmented_H, augmented_W)
 
         source_LQ = torch.Tensor(source_LQ).unsqueeze(0).to(device)
         source_HQ = torch.Tensor(source_HQ).unsqueeze(0).to(device)
+        target_HQ = torch.Tensor(target_HQ).unsqueeze(0).to(device)
 
         with torch.no_grad():
             kernel_mean, kernel_sigma = model(source_HQ, source_LQ)
             kernel = kernel_mean + kernel_sigma * torch.randn_like(kernel_mean)
-            fake_LQ = model.adaptKernel(source_HQ, kernel)
+            fake_source_LQ = model.adaptKernel(source_HQ, kernel)
+            target_LQ = model.adaptKernel(target_HQ, kernel)
 
         LQ_img = util.tensor2img(source_LQ)
-        fake_LQ_img = util.tensor2img(fake_LQ)
+        fake_LQ_img = util.tensor2img(fake_source_LQ)
+        target_LQ_img = util.tensor2img(target_LQ)
+        target_HQ_img = util.tensor2img(target_HQ)
+
+        target_HQ_dst = osp.join(save_path, "sharp/{:03d}/{:08d}.png")
+        target_LQ_dst = osp.join(save_path, "blur/{:03d}/{:08d}.png")
+
+        os.makedirs(os.dirname(target_HQ_dst), exist_ok=True)
+        os.makedirs(os.dirname(target_LQ_dst), exist_ok=True)
+
+        cv2.imwrite(target_HQ_dst, target_HQ_img)
+        cv2.imwrite(target_LQ_dst, target_LQ_img)
 
         psnr = util.calculate_psnr(LQ_img, fake_LQ_img)
 
-        print("PSNR: {:.2f}db".format(psnr))
+        logger.info("Reconstruction PSNR of image #{:03d}/{:03d}: {:.2f}db".format(i, num_images, psnr))
         psnr_avg += psnr
 
-    print("Average PSNR: {:.2f}db".format(psnr_avg / num_samples))
+    logger.info("Average reconstruction PSNR: {:.2f}db".format(psnr_avg / num_images))
 
 
 main()
